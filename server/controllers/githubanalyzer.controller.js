f83d4dbc744763127a52179641190164ca32c66a
Diego Luiz Brum <diego.l.brum@outlook.com>
2017-06-02 22:06:39 +0100

    Added GiHubClient (from https://github.com/github/platform-samples/api/javascript/es2015-nodejs)

---
+++ server/controllers/githubanalyzer.controller.js
@@ -0,0 +1,15 @@
+
+/**
+ * Get all repositories of informed user
+ * @param req
+ * @param res
+ * @returns void
+ */
+export function getRepos(req, res) {
+    req.params.user;
+
+    // if (err) {
+    //     res.status(500).send(err);
+    // }
+    res.json({});
+}
--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import * as PostController from '../controllers/post.controller';
+import * as GitHubAnalyzerController from '../controllers/githubanalyzer.controller';
 const router = new Router();
 
 // Get all Posts
@@ -13,5 +14,7 @@ router.route('/posts').post(PostController.addPost);
 
 // Delete a post by cuid
 router.route('/posts/:cuid').delete(PostController.deletePost);
+
+router.route('analyzer/:user/github').get(GitHubAnalyzerController.getRepos);
 
 export default router;
