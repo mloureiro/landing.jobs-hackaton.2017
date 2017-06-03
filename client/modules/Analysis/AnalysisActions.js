5b9006e632affdf83ccbcaf094624c4db7ea7e03
Nuno Silva <nuno.levezinho.silva@mobilityoneit.onmicrosoft.com>
2017-06-03 15:57:15 +0100

    Added fetch score action

--- client/modules/Analysis/AnalysisActions.js
+++ client/modules/Analysis/AnalysisActions.js
@@ -2,12 +2,26 @@ import callApi from '../../util/apiCaller';
 
 // Export Constants
 export const ADD_RESULT = 'ADD_RESULT';
+export const ADD_SCORE = 'ADD_SCORE';
 
 // Export Actions
 export function addResult(result) {
   return {
     type: ADD_RESULT,
     result,
+  };
+}
+
+export function addScore(result) {
+  return {
+    type: ADD_SCORE,
+    result
+  };
+}
+
+export function fetchScore(username) {
+  return (dispatch) => {
+    return callApi(`analyzer/${username}/github/score`).then(res => dispatch(addScore(res.content)));
   };
 }
 
