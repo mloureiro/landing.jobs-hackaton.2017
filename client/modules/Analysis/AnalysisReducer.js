e2fc4afdda66faaff929b1d1b6a8bfa394e192a7
Luis Carneiro <eltugis@gmail.com>
2017-06-03 03:15:55 +0100

    Initial client implementations based on starter kit

---
+++ client/modules/Analysis/AnalysisReducer.js
@@ -0,0 +1,24 @@
+import { ADD_RESULT } from './AnalysisActions';
+
+// Initial State
+const initialState = { data: [] };
+
+const AnalysisReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_RESULT :
+      return {
+        data: [action.result, ...state.data],
+      };
+
+    default:
+      return state;
+  }
+};
+
+/* Selectors */
+
+// Get result by username
+export const getResult = (state, username) => state.results.data.filter(result => result.username === username)[0];
+
+// Export Reducer
+export default AnalysisReducer;
